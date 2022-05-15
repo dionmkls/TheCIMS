2f7fd94290aa07a098726affdf691bc513323a17
Visianita Widyaningrum <visianitawidya@gmail.com>
2022-05-15 12:04:52 +0700

    urls

--- tkbasdat/urls.py
+++ tkbasdat/urls.py
@@ -20,14 +20,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('login/', include('login.urls')),
-    path('tokoh/', include('tokoh.urls')),
-    path('makan/', include('makan.urls')),
-    path('makanan/', include('makanan.urls'))
     path('kategori_apparel/', include('kategori_apparel.urls')),
     path('koleksi/', include('koleksi.urls')),
     path('koleksi_tokoh/', include('koleksi_tokoh.urls')),
     path('tokoh/', include('tokoh.urls')),
+    path('warna_kulit/', include('warna_kulit.urls')),
+    path('level/', include('level.urls')),
+    path('menggunakan_apparel/', include('menggunakan_apparel.urls')),
     path('bekerja/', include('bekerja.urls')),
     path('pekerjaan/', include('pekerjaan.urls')),
->>>>>>> tkbasdat/urls.py
+    path('makan/', include('makan.urls')),
+    path('makanan/', include('makanan.urls'))
 ]
+
